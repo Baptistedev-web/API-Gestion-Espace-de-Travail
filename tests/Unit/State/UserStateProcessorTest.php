f02f5a91603d6ae4caa38439660184c5fc9b5c1b
Digonnet Baptiste <154701055+Baptistedev-web@users.noreply.github.com>
2025-05-11 10:33:06 +0200

    EDT-7 Entité EspaceTravail + entité fille + entité en raltion avec entité fille + teste

--- tests/Unit/State/UserStateProcessorTest.php
+++ tests/Unit/State/UserStateProcessorTest.php
@@ -6,9 +6,11 @@ use App\Entity\User;
 use App\Security\PasswordHasher;
 use App\State\UserStateProcessor;
 use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use ApiPlatform\Metadata\Operation;
 
+#[ CoversClass(UserStateProcessor::class) ]
 class UserStateProcessorTest extends TestCase
 {
     /** @phpstan-ignore-next-line */
@@ -107,6 +109,83 @@ class UserStateProcessorTest extends TestCase
         $result = $this->processor->process($user, $this->createMock(Operation::class));
 
         $this->assertEquals('existing_hashed_password', $user->getPassword());
+        $this->assertInstanceOf(User::class, $result);
+    }
+    public function testProcessThrowsExceptionWhenUserNotFound(): void
+    {
+        $user = new User();
+        $reflection = new \ReflectionClass($user);
+        $property = $reflection->getProperty('id');
+        $property->setAccessible(true);
+        $property->setValue($user, 1); // Simule un ID défini par Doctrine
+
+        $userRepository = $this->createMock(\Doctrine\ORM\EntityRepository::class);
+
+        $userRepository
+            ->expects($this->once())
+            ->method('find')
+            ->with(1)
+            ->willReturn(null); // Simule un utilisateur non trouvé
+
+        /** @phpstan-ignore-next-line */
+        $this->entityManager
+            ->method('getRepository')
+            ->with(User::class)
+            ->willReturn($userRepository);
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Utilisateur non trouvé');
+
+        $this->processor->process($user, $this->createMock(Operation::class));
+    }
+
+    public function testProcessUpdatesUserWithNewPassword(): void
+    {
+        $user = new User();
+        $reflection = new \ReflectionClass($user);
+        $property = $reflection->getProperty('id');
+        $property->setAccessible(true);
+        $property->setValue($user, 1); // Simule un ID défini par Doctrine
+        $user->setPlainPassword('new_password');
+
+        $existingUser = new User();
+        $existingUser->setPassword('existing_hashed_password');
+
+        $userRepository = $this->createMock(\Doctrine\ORM\EntityRepository::class);
+
+        $userRepository
+            ->expects($this->once())
+            ->method('find')
+            ->with(1)
+            ->willReturn($existingUser);
+
+        /** @phpstan-ignore-next-line */
+        $this->entityManager
+            ->method('getRepository')
+            ->with(User::class)
+            ->willReturn($userRepository);
+
+        /** @phpstan-ignore-next-line */
+        $this->passwordHasher
+            ->expects($this->once())
+            ->method('hashPassword')
+            ->with($user, 'new_password')
+            ->willReturn('new_hashed_password');
+
+        /** @phpstan-ignore-next-line */
+        $this->entityManager
+            ->expects($this->once())
+            ->method('persist')
+            ->with($user);
+
+        /** @phpstan-ignore-next-line */
+        $this->entityManager
+            ->expects($this->once())
+            ->method('flush');
+
+        $result = $this->processor->process($user, $this->createMock(Operation::class));
+
+        $this->assertEquals('new_hashed_password', $user->getPassword());
         $this->assertInstanceOf(User::class, $result);
     }
 }
